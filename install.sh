e0d26551844f9c08397063305a0bfbd86c4f3b12
Tomáš Batelka <tomas.batelka@email.cz>
2021-08-25 03:25:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\n# Disable rainbow image at boot\ndisable_splash=1" >> /boot/config.txt
 echo -e "startx --" >> ~/.bashrc
-sudo apt purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 smartsim java-common minecraft-pi libreoffice* lxde lxpanel lxdm lxde-common -y
+sudo apt purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 smartsim java-common minecraft-pi libreoffice* geany python3-thonny dillo vlc -y
 
 sudo apt clean
 sudo apt autoremove -y
@@ -9,10 +9,9 @@ sudo apt autoremove -y
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt-get install xdotool unclutter sed matchbox-window-manager -y
+sudo apt-get install xdotool unclutter sed chromium-browser -y
 
-sudo cp kiosk.sh ~/
-sudo cp kiosk.service /lib/systemd/system/
+sudo cp -f kiosk.sh ~/
 sudo cp -r themes/spos /usr/share/plymouth/themes/
 
 sudo plymouth-set-default-theme spos -R

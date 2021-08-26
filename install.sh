771bcd6cfedd6437236a9d549f4bd550a443e8f7
Tomáš Batelka <tomas.batelka@email.cz>
2021-08-26 11:56:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Disable rainbow image at boot
-echo -e "\n# Disable rainbow image at boot\ndisable_splash=1" >> /boot/config.txt
-
-# Run startx script
-sudo sed -i "`wc -l < /etc/rc.local` i\\\n# Run startx script\nstartx\n" /etc/rc.local
-
 # Remove unnecessary packages
 sudo apt purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 smartsim java-common minecraft-pi libreoffice* geany python3-thonny dillo vlc -y
 
@@ -16,9 +10,21 @@ sudo apt update
 
 sudo apt-get install xdotool unclutter sed chromium-browser lightdm openbox xorg rpd-plym-splash -y
 
-# Copy files
+# Copy config files
 sudo cp -f .xsessionrc ~/
 sudo cp -r themes/spos /usr/share/plymouth/themes/
 
 # Apply theme
 sudo plymouth-set-default-theme spos -R
+
+# Disable rainbow image at boot
+echo -e "\n# Disable rainbow image at boot\ndisable_splash=1" >> /boot/config.txt
+
+# Run startx script
+sudo sed -i "`wc -l < /etc/rc.local` i\\\n# Run startx script\nstartx\n" /etc/rc.local
+
+# Enable plymouth splashscreen using raspi-config script
+sudo raspi-config nonint do_boot_splash 0
+
+# Enable autologin using raspi-config script
+sudo raspi-config nonint do_boot_behaviour B4
